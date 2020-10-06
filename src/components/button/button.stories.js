858077ce8628a5319efa5c27c0a4214ca83c1b10
Matthias Heisterkamp <hello@mhstrkmp.dev>
2020-10-06 20:26:53 +0200

    Add button content

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,8 +1,10 @@
 import "./button.css";
 import buttonPrimary from "./button-primary.html";
 import buttonSecondary from "./button-secondary.html";
+import buttonContent from "./button-content.html";
 
 export default { title: "Components/Button" };
 
 export const primary = () => buttonPrimary;
 export const secondary = () => buttonSecondary;
+export const content = () => buttonContent;
