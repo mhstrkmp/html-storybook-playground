a7d3854808a0e1955a9df9033d1a78ece7bb2daf
Matthias Heisterkamp <hello@mhstrkmp.dev>
2020-10-08 16:54:45 +0200

    Add basic app layout

---
+++ src/components/appPageTemplate/app-page-template.stories.js
@@ -0,0 +1,6 @@
+import "./app-page-template.css";
+import appPageTemplate from "./app-page-template.html";
+
+export default { title: "Components/App" };
+
+export const page = () => appPageTemplate;
